6056d1993ef96bf2ae6f163457384d352fb11f66
sushinectar <lucianoxjktop@gmail.com>
2024-12-12 17:43:09 -0300

    added media queries

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,80 @@
 "use client"
 
+import "./media.css"
+
 import React, { useState, useEffect } from "react"
-import { getWeather } from "@/lib/weather"
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useTheme } from "next-themes"
+import { ThemeProvider } from "@/components/ui/theme-provider"
+import { Moon, Sun } from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+
+import { getWeather } from "@/lib/weather"
+import { getGeo } from "@/lib/geo"
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 export default function Home() {
   const [city, setCity] = useState("São Paulo")
   const [weather, setWeather] = useState(null)
   const [error, setError] = useState(null)
+  const { theme, setTheme } = useTheme()
 
   // Mapping descriptions to image URLs
   const weatherImages = {
     "céu limpo": "/sunny.png",
     "nuvens quebradas": "/break-cloud.png",
     "nuvens dispersas": "/break-cloud.png",
-    "chuva fraca": "/images/short-rain.png",
+    "chuva fraca": "/short-rain.png",
     "céu pouco nublado": "/cloudy.png",
     "neve fraca": "/snow.png",
     nublado: "/cloudy.png",
+    trovoada: "/lightning.png",
+  }
+
+  // Capitalize the first letter of a string
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
   }
 
   useEffect(() => {
+    // Fetch user's geolocation and update the city
+    async function fetchGeoAndWeather() {
+      try {
+        const geoData = await getGeo()
+        if (geoData && geoData.city) {
+          setCity(geoData.city) // Update the name of the detected city
+        }
+      } catch (error) {
+        console.error("Erro ao obter localização:", error)
+      }
+    }
+
+    fetchGeoAndWeather()
+  }, [])
+
+  useEffect(() => {
+    // Fetch weather data whenever the city changes
     async function fetchWeather() {
       try {
         const data = await getWeather(city)
@@ -35,67 +89,91 @@ export default function Home() {
   }, [city])
 
   return (
-    <main className="flex flex-col justify-center align-center text-center item-center p-4 gap-4">
-      <h1 className="text-2xl font-bold">Clima Atual</h1>
-      <div className="mx-2 p-4 border-2 rounded-lg">
-        <Input
-          type="text"
-          value={city}
-          onChange={e => setCity(e.target.value)}
-          className="mt-4 p-2 border"
-          placeholder="Digite uma cidade"
-        />
-        {error && <p className="text-red-500">{error}</p>}
-        {weather && (
-          <div className="flex flex-col items-center mt-4">
-            <div className="flex flex-col mt-2 mb-6 gap-2">
-              <div className="flex justify-center">
-                {/* Bandeira do país */}
-                <div className="text-sm">
+    <ThemeProvider>
+      <main className="flex flex-col text-center p-4 gap-4 transition-colors duration-500 ease-in-out">
+        <nav className="flex justify-around items-center my-4">
+          <h1 className="text-2xl font-bold">Weather App</h1>
+          <ModeToggle /> {/* Replace the button with the ModeToggle component */}
+        </nav>
+        <div className="box-container mx-2 p-4 border border-zinc-200 rounded-lg shadow-md shadow-zinc-400 transition-all duration-500 ease-in-out">
+          <div>
+            <span className="search-img relative top-custom left-4 pointer-events-none">
+              <img src="/search.png" alt="Lupa" className="w-4 h-4 filter dark:invert" />
+            </span>
+            <Input
+              type="text"
+              value={city}
+              onChange={e => setCity(e.target.value)}
+              className="mt-4 pb-6 pl-14 pt-6 border border-zinc-400"
+              placeholder="Digite uma cidade"
+            />
+          </div>
+          {error && <p className="text-red-500">{error}</p>}
+          {weather && (
+            <div className="flex flex-col items-center mt-4">
+              <div className="flex flex-col mt-8 mb-6 gap-2">
+                <div className="flex justify-center">
+                  {/* Country flag */}
+                  <div className="text-sm">
+                    <img
+                      src={`https://flagsapi.com/${weather.sys.country}/flat/64.png`}
+                      width={30}
+                      height={30}
+                      alt="Bandeira do país"
+                    />
+                  </div>
+                  <h2 className="text-lg mx-2 mb-2">{weather.name}</h2>
+                </div>
+                {/* Weather image display */}
+                <div className="flex flex-col items-center mt-6">
                   <img
-                    src={`https://flagsapi.com/${weather.sys.country}/flat/64.png`}
-                    width={30}
-                    height={30}
-                    alt="Bandeira do país"
+                    src={weatherImages[weather.weather[0].description]}
+                    alt="Imagem do clima"
+                    width={100}
+                    height={100}
+                    className="filter dark:invert"
                   />
                 </div>
-                <h2 className="text-lg mx-2">{weather.name}</h2>
+                {/* Temperature */}
+                <p className="text-4xl mt-2">{Math.round(weather.main.temp)}°C</p>
+                {/* Weather description */}
+                <div className="text-sm text-zinc-700 my-2">
+                  {capitalizeFirstLetter(weather.weather[0].description)}
+                </div>
               </div>
-              {/* Weather image display */}
-              <div className="flex flex-col justify-center mt-4">
-                <img
-                  src={weatherImages[weather.weather[0].description]}
-                  alt="Imagem do clima"
-                  width={100}
-                  height={100}
-                />
+              <div className="flex flex-col items-center w-72 pb-4 pt-6 mb-4">
+                {/* Maximum, minimum temperatures and humidity */}
+                <div className="flex gap-14">
+                  <div className="flex flex-col items-center gap-2 text-sm text-zinc-600">
+                    <img
+                      src="/max.png"
+                      alt="Temperatura maxima"
+                      width={30}
+                      height={30}
+                      className="filter dark:invert"
+                    />
+                    {Math.round(weather.main.temp_max)}°C
+                  </div>
+                  <div className="flex flex-col items-center gap-2 text-sm text-zinc-600">
+                    <img
+                      src="/min.png"
+                      alt="Temperatura minima"
+                      width={30}
+                      height={30}
+                      className="filter dark:invert"
+                    />
+                    {Math.round(weather.main.temp_min)}°C
+                  </div>
+                  <div className="flex flex-col items-center gap-2 text-sm text-zinc-600">
+                    <img src="humidity.png" alt="Umidade" width={30} height={30} className="filter dark:invert" />
+                    {weather.main.humidity}%
+                  </div>
+                </div>
               </div>
-              {/* Temperatura */}
-              <p className="text-4xl mt-2">{Math.round(weather.main.temp)}°C</p>
-              {/* Weather description */}
-              <div className="text-sm">{weather.weather[0].description}</div>
             </div>
-            <div className="flex flex-col items-center w-72 pb-4 pt-6 mb-4 border rounded-md">
-              {/* Temperaturas máxima, mínima e umidade */}
-              <div className="flex gap-12">
-                <div className="flex flex-col items-center gap-2 text-md">
-                  <img src="/maximum.png" alt="Icon" width={40} height={40} />
-                  {Math.round(weather.main.temp_max)}°C
-                </div>
-                <div className="flex flex-col items-center gap-2 text-md">
-                  <img src="/minimum.png" alt="Icon" width={40} height={40} />
-                  {Math.round(weather.main.temp_min)}°C
-                </div>
-                {/* Umidade e descrição */}
-                <div className="flex flex-col items-center gap-2 text-md">
-                  <img src="humidity.png" alt="Icon" width={40} height={40} />
-                  {weather.main.humidity}%
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </main>
+          )}
+        </div>
+      </main>
+    </ThemeProvider>
   )
 }
